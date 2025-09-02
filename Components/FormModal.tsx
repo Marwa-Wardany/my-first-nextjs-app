b83db8795196fb377901a92ede23904555fc7286
Marwa Mohammed Yousef <marwawardany9@gmail.com>
2025-09-02 12:01:25 +0300

    Update FormModal.tsx

--- Components/FormModal.tsx
+++ Components/FormModal.tsx
@@ -8,7 +8,7 @@ export default function FormModal({ isVisible, errorMessage }: FormModalProps){
         <div className="form-modal position-absolute w-100 h-100 d-flex justify-content-center align-items-center" style={{top: "78px"}}>
             <div className="bg-gradient-to-r to-gray-400 from-gray-50 w-50 rounded-2xl shadow-2xl text-center">
                 <h2 className= {`p-2 ${errorMessage ? "text-danger" : "blue-text"}`}>
-                    {errorMessage != null ? errorMessage : 'Form has been submitted successfuly' }
+                    {errorMessage != null ? errorMessage : 'تم ارسال طلبك سيتم التواصل معك في اقرب وقت' }
                 </h2>
             </div>
         </div>
